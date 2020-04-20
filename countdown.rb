d4fdd5321b8e386a770f93da84e77c4d690a20b7
Ifeoluwa  Akinremi-Wade <ifeoluwa.wade@gmail.com>
2020-04-20 22:46:32 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,37 +1,16 @@
-def countdown(number)
-  while number > 0
-    puts "#{10} SECOND(S)!"
-    numeber -= 1
-    "HAPPY NEW YEAR!"
-
+def countdown(n)
+  while n > 0
+    puts "#{n} SECOND(S)!"
+    n -= 1
   end
+  "HAPPY NEW YEAR!"
 end
 
-
-
-
-
-
-
-
-
-
-
-
-
-# def countdown(n)
-#   while n > 0
-#     puts "#{n} SECOND(S)!"
-#     n -= 1
-#   end
-#   "HAPPY NEW YEAR!"
-# end
-#
-# def countdown_with_sleep(n)
-#   while n > 0
-#     puts "#{n} SECOND(S)!"
-#     n -= 1
-#     sleep 1
-#   end
-#   "HAPPY NEW YEAR!"
-# end
+def countdown_with_sleep(n)
+  while n > 0
+    puts "#{n} SECOND(S)!"
+    n -= 1
+    sleep 1
+  end
+  "HAPPY NEW YEAR!"
+end
