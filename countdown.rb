1b039c16ebe59f8d5bac572c4fa6778d5f02b649
Ifeoluwa  Akinremi-Wade <ifeoluwa.wade@gmail.com>
2020-04-20 22:36:17 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,18 +1,36 @@
-#write your code here
+def loop_countdown(number)
+  number > 0
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    numeber -= 1
 
-def countdown(n)
-  while n > 0 
-    puts "#{n} SECOND(S)!"
-    n -= 1
   end
-  "HAPPY NEW YEAR!"
-end
 
-def countdown_with_sleep(n)
-  while n > 0 
-    puts "#{n} SECOND(S)!"
-    n -= 1
-    sleep 1
-  end
-  "HAPPY NEW YEAR!"
-end
+
+
+
+
+
+
+
+
+
+
+
+
+# def countdown(n)
+#   while n > 0
+#     puts "#{n} SECOND(S)!"
+#     n -= 1
+#   end
+#   "HAPPY NEW YEAR!"
+# end
+#
+# def countdown_with_sleep(n)
+#   while n > 0
+#     puts "#{n} SECOND(S)!"
+#     n -= 1
+#     sleep 1
+#   end
+#   "HAPPY NEW YEAR!"
+# end
